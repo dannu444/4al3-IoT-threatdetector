2838bfa2d0cd1e2666ce85f3f38bdc5b6ad0a180
Jacob-MB <brodersj@mcmaster.ca>
2025-12-04 17:09:27 -0500

    Added Upsampling to testing.py

--- testing.py
+++ testing.py
@@ -14,6 +14,8 @@ import train
 from argparse import ArgumentParser 
 from itertools import product
 
+from imblearn.over_sampling import RandomOverSampler
+
 def set_all_seeds(seed):
         random.seed(seed)
         np.random.seed(seed)
@@ -205,6 +207,28 @@ def main():
     y_val = target[train_end:val_end, :]
     X_test = input[val_end:, :]
     y_test = target[val_end:, :]
+
+    ros = RandomOverSampler(random_state=1234)
+    X_train, y_train = ros.fit_resample(X_train, y_train)
+
+    # Check for classes being equally distributed
+    test = {0: 0,
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 0,
+            5: 0,
+            6: 0,
+            7: 0,
+            8: 0,
+            9: 0,
+            10: 0,
+            11: 0}
+    for i in range(y_train.shape[0]):
+        for j in range(y_train.shape[1]):
+            if y_train[i][j] > 0:
+                test[j] += 1
+    print(test)
 
     # Create tensors for data
     X_train_t = torch.tensor(X_train).float()
